921a592679a69f81fbb3344e24bf7e02bf9a663e
chenrog <roger.chen@acquia.com>
2023-04-08 14:55:29 -0400

    setup complete

--- main.go
+++ main.go
@@ -1,15 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
 
 type Player struct {
 	Cards  []int
 	Tokens int
 }
 
-func main() {
-	var cards []int
-	cards = append(cards, 1)
+func NewPlayer(tokens int) *Player {
+	return &Player{Tokens: tokens}
+}
 
-	fmt.Println(cards)
+func main() {
+	var deck []int
+	var players []Player
+
+	// DECK SETUP
+	for i := 3; i <= 35; i++ {
+		deck = append(deck, i)
+	}
+
+	rand.New(rand.NewSource(time.Now().UnixNano()))
+	rand.Shuffle(len(deck), func(i, j int) { deck[i], deck[j] = deck[j], deck[i] })
+
+	deck = deck[9:]
+
+	// PLAYER SETUP
+	for i := 0; i < 3; i++ {
+		players = append(players, *NewPlayer(11))
+	}
+
+	fmt.Println("deck: ", deck)
+	fmt.Println("deck count: ", len(deck))
+	fmt.Println("players: ", players)
 }
