89a6d4cfb320666a76950cd367a2bc4d741658d1
chenrog <roger.chen@acquia.com>
2023-05-02 21:33:37 -0400

    basic websocket with melody

--- server/main.go
+++ server/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"github.com/olahol/melody"
+	"log"
+	"net/http"
 )
 
 func main() {
@@ -13,4 +16,30 @@ func main() {
 	game.Action(Pass)
 	game.Action(Take)
 	fmt.Println(game)
+
+	m := melody.New()
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "index.html")
+	})
+
+	http.HandleFunc("/ws", func(w http.ResponseWriter, r *http.Request) {
+		err := m.HandleRequest(w, r)
+		if err != nil {
+			return
+		}
+	})
+
+	m.HandleMessage(func(s *melody.Session, msg []byte) {
+		log.Println(string(msg))
+		err := m.Broadcast(msg)
+		if err != nil {
+			return
+		}
+	})
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return
+	}
 }
